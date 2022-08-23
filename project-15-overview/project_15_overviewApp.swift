66de70781fc2471d424c6d2eaea1d9a4aaec8742
adarsh <adarsh0ranjan@gmail.com>
2022-08-23 10:35:37 +0530

    Initial Commit

---
+++ project-15-overview/project_15_overviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_15_overviewApp.swift
+//  project-15-overview
+//
+//  Created by Roro Solutions on 23/08/22.
+//
+
+import SwiftUI
+
+@main
+struct project_15_overviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
